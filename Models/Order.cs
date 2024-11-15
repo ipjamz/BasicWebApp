38f5788081410930be59e120f3b112d3bd3a088e
Peter James Babiera <pjames20.babiera@gmail.com>
2024-11-15 22:58:12 +0800

    Fix Order.cs data types. Made not required in completed Order.cs that cause error cause of a null value. Fix table layout to loop items list.

--- Models/Order.cs
+++ Models/Order.cs
@@ -4,13 +4,13 @@ public class Order
 { 
       public int id { get; set; }
       public string status { get; set; }
-      public string paymenthMethod { get; set; }
-      public string recruiterId { get; set; }
-      public string divisionId { get; set; }
-      public string companyId { get; set; }
-      public string validated { get; set; }
+      public string paymentMethod { get; set; }
+      public int recruiterId { get; set; }
+      public int divisionId { get; set; }
+      public int companyId { get; set; }
+      public bool validated { get; set; }
       public string created { get; set; }
-      public string completed { get; set; } 
+      public string? completed { get; set; } 
       public Item[] items { get; set; }
 }
 
